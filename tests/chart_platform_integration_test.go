f94d5dc64353c14821537ed9761f7e08a2500aeb
Sean Trantalis <strantalis@virtru.com>
2024-12-04 12:48:00 -0500

    fix: set appVersion to v0.4.32 (#92)

--- tests/chart_platform_integration_test.go
+++ tests/chart_platform_integration_test.go
@@ -165,7 +165,7 @@ func (suite *PlatformChartIntegrationSuite) TestBasicDeployment() {
 
 	kcDataPath, err := filepath.Abs("../platform/service/cmd/keycloak_data.yaml")
 	suite.Require().NoError(err)
-	dockerRun := exec.Command("docker", "run", "--rm", "--network=platform-k3d", "--add-host=keycloak.opentdf.local:10.255.127.1", "-v", fmt.Sprintf("%s:/keycloak_data.yaml", kcDataPath), "registry.opentdf.io/platform:nightly", "provision", "keycloak", "-p", kcAdminPass, "-e", "https://keycloak.opentdf.local", "-f", "/keycloak_data.yaml")
+	dockerRun := exec.Command("docker", "run", "--rm", "--network=platform-k3d", "--add-host=keycloak.opentdf.local:10.255.127.1", "-v", fmt.Sprintf("%s:/keycloak_data.yaml", kcDataPath), "registry.opentdf.io/platform:latest", "provision", "keycloak", "-p", kcAdminPass, "-e", "https://keycloak.opentdf.local", "-f", "/keycloak_data.yaml")
 	dockerRunOutput, err := dockerRun.CombinedOutput()
 	suite.Require().NoError(err, string(dockerRunOutput))
 	if err == nil {
